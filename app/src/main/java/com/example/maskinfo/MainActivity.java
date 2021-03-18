5fad48b3f52f9e27fdb46c06d11f014d38b64e64
stevey-sy <sinsy89@hanmail.net>
2021-03-18 18:31:39 +0900

    [MODIFY] 리사이클러뷰에 적용할 adpater 클래스 추가, Retrofit 연동 성공

--- app/src/main/java/com/example/maskinfo/MainActivity.java
+++ app/src/main/java/com/example/maskinfo/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.maskinfo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.maskinfo.model.Store;
+import com.example.maskinfo.model.StoreInfo;
+import com.example.maskinfo.repository.MaskService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        final StoreAdapter adapter = new StoreAdapter();
+        recyclerView.setAdapter(adapter);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MaskService.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+        MaskService service = retrofit.create(MaskService.class);
+
+        Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
+        // 안드로이드 에서는 네트워크 관련 메소드를 실행할 때 비동기식으로 처리해야만 작동한다.
+        // .execute()는 동기식을 지원하는 메소드이기 때문에 바꿔야한다.
+//        StoreInfo storeInfo = storeInfoCall.execute().body();
+
+        // .execute() 대신에 enqueue()를 사용해야 한다.
+        storeInfoCall.enqueue(new Callback<StoreInfo>() {
+            @Override
+            public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
+                List<Store> items = response.body().getStores();
+                // 외부에서 선언된 변수를 사용할 경우 에러가 난다.
+                // 이유는 변경이 가능한 객체이기 때문에
+                adapter.updateItems(items);
+            }
+
+            @Override
+            public void onFailure(Call<StoreInfo> call, Throwable t) {
+                Log.e(TAG, "onFailure: ", t );
+            }
+        });
     }
+}
+
+class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreViewHolder> {
+
+    private List<Store> mItems = new ArrayList<>();
+
+    // view holder 를 만드는 부분
+    @NonNull
+    @Override
+    public StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_store, parent, false);
+        return new StoreViewHolder(view);
+    }
+
+    // View Holder 에 데이터를 bind 하는 부분
+    @Override
+    public void onBindViewHolder(@NonNull StoreViewHolder holder, int position) {
+        Store store = mItems.get(position);
+
+        holder.nameTextView.setText(store.getName());
+        holder.addressTextView.setText(store.getAddr());
+        holder.distanceTextView.setText("1.0 Km");
+        holder.remainTextView.setText(store.getRemainStat());
+        holder.countTextView.setText("100개 이상");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    //  item view 의 정보를 가지고 있는 클래스
+    static class StoreViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView;
+        TextView addressTextView;
+        TextView distanceTextView;
+        TextView remainTextView;
+        TextView countTextView;
+
+        public StoreViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.name_text_view);
+            addressTextView = itemView.findViewById(R.id.addr_text_view);
+            distanceTextView = itemView.findViewById(R.id.distance_text_view);
+            remainTextView = itemView.findViewById(R.id.remain_text_view);
+            countTextView = itemView.findViewById(R.id.count_text_view);
+        }
+    }
+
+    public void updateItems(List<Store> items) {
+        mItems = items;
+        notifyDataSetChanged();
+    }
+
 }
